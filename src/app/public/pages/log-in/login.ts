3fa438ff8f46acc9518de069a0e0d2a0ccbed1c5
= <=>
2026-06-11 12:46:16 +0300

    fix threads

--- src/app/public/pages/log-in/login.ts
+++ src/app/public/pages/log-in/login.ts
@@ -3,8 +3,8 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Icons } from '../../../../shared/icons';
 import { Images } from '../../../../shared/images';
 import { NAV_CONST } from './constants';
-import { IMenu } from '../../component/nav-button/models';
-import { NavButtonComponent } from '../../component/nav-button/nav-button';
+import { IMenu } from '../../../private/component/nav-button/models';
+import { NavButtonComponent } from '../../../private/component/nav-button/nav-button';
 
 @Component({
     selector: 'app-login',
