0fd0c90e461e1e004de75568190a942f2a82e181
= <=>
2026-06-11 13:25:37 +0300

    fix threads

--- src/app/private/constants.ts
+++ src/app/private/constants.ts
@@ -5,14 +5,14 @@ export const NAV_CONST: IMenu[] = [
         text: 'Главная',
         iconUrl: '/icons/nav-home.svg',
         iconUrlActive: 'menu/dashboard-active.svg',
-        url: 'private/home',
+        url: 'phome',
         id: 0,
     },
     {
         text: 'Избранное',
         iconUrl: '/icons/nav-start.svg',
         iconUrlActive: 'menu/wallet-active.svg',
-        url: '/private/favorites',
+        url: 'favorites',
         id: 1,
     },
 ];
