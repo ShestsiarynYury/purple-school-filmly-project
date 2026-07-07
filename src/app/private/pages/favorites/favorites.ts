1d63a66aef920d9d7ca15f70175d4e9b5b733a86
= <=>
2026-07-07 14:35:58 +0300

    fix threads

--- src/app/private/pages/favorites/favorites.ts
+++ src/app/private/pages/favorites/favorites.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FAVORITES } from '../../../shared/const/fake-favorites.cont';
 import { CardComponent } from '../../components/card/card.component';
 import { delay, of } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'app-favorites',
@@ -9,7 +10,7 @@ import { delay, of } from 'rxjs';
     styleUrl: './favorites.scss',
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [CardComponent],
+    imports: [CardComponent, AsyncPipe],
 })
 export class FavoritesComponent {
     // favoriteMovies = input<IMovie[]>(FAVORITES);
